6b66985cc53f1cfabfe6a0337f08b91d9008f31d
Nikola Knežević <knezevicdev@gmail.com>
2021-02-10 21:19:48 +0100

    Password attempts limit

--- includes/check_password.php
+++ includes/check_password.php
@@ -9,9 +9,25 @@ include_once __DIR__ . '/../includes/get_secret.php';
 $invalid_Password = false;
 $is_post = false;
 
+if (isset($_SESSION['password_attempts']) &&
+    isset($_SESSION['password_attempts'][$secret->id]) &&
+    $_SESSION['password_attempts'][$secret->id] > 3) {
+    end_with_message('You have been reached the attempts limit.', 'Error');
+}
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if ($secret->hasPassword() && !$secret->verifyPassword($_POST['password'])) {
         $invalid_Password = true;
+        
+        if (!isset($_SESSION['password_attempts'])) {
+            $_SESSION['password_attempts'] = [];
+        }
+
+        if (!isset($_SESSION['password_attempts'][$secret->id])) {
+            $_SESSION['password_attempts'][$secret->id] = 0;
+        }
+
+        $_SESSION['password_attempts'][$secret->id]++;
     } else {
         $db->deleteSecret($secret->id);
         $is_post = true;
