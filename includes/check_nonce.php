0a465b0825f3636c24c3d712351e2a447ad81bdf
Nikola Knežević <knezevicdev@gmail.com>
2021-02-10 17:03:48 +0100

    Nonce checking fix

--- includes/check_nonce.php
+++ includes/check_nonce.php
@@ -8,7 +8,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     }
 
     $found_nonce = array_search($_POST['nonce'], $_SESSION['nonce']);
-    if (!$found_nonce) {
+    if ($found_nonce === false) {
         end_with_message("Invalid request.");
     }
     unset($_SESSION['nonce'][$found_nonce]);
@@ -18,4 +18,5 @@ use Ramsey\Uuid\Uuid;
 
 $nonce = Uuid::uuid4();
 
-$_SESSION['nonce'] [] = $nonce;
+if (!isset($_SESSION['nonce'])) $_SESSION['nonce'] = [];
+$_SESSION['nonce'] [] = $nonce->toString();
